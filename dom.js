90de5207d64e24f9e5c5e327623c26498c17c246
andy <urbanwhaleshark@gmail.com>
2012-11-20 21:29:57 +0000

    Updated code

--- dom.js
+++ dom.js
@@ -1,31 +1,57 @@
-// Revisited the SP test.
+(function () {
 
-var obj = [{pName: 'p1', pCode: 'abcd', mVal: 0}, {pName: 'p2', pCode: 'dcba', mVal: 11}, {pName: 'p1', pCode: 'abcd', mVal: -21}]
-var el = function(id) { return document.getElementById(id) }
+  var
+    obj = [
+      {pName: 'Andy Willis', pCode: '1234-56778', mVal: 10045},
+      {pName: 'Barney Stintson', pCode: '00-8764-10', mVal: -1078},
+      {pName: 'James Corvel', pCode: '34-4567-8700-0', mVal: 32},
+      {pName: 'Bernard Bresslaw', pCode: '1234-56778', mVal: 10045},
+      {pName: 'Tarzan', pCode: '00-8764-10', mVal: 1078},
+      {pName: 'Mr. Machine', pCode: '34-4567-8700-0', mVal: -132}
+      ],
+    el = function (id) { return document.getElementById(id); };
 
-// Click link and display data
-el('link').onclick = function() { displayData(); }
+  // Click link, remove any existing data,
+  // then - and only then - display data (uses a callback)
+  el('link').addEventListener('click', function () {
+    clearData(function () {
+      displayData();
+    });
+  }, false);
 
-// Build table by creating a docfrag first, then pinning it to the tree.
-function displayData() {
-  // Create fragment and append a table element to it
-  // html and color are used in the loop
-  var frag = document.createDocumentFragment('frag')
-    , table = document.createElement('table')
-    , html, color
-    ;
-
-  // Loop over data adding to the html var
-  // changing the color appropriate to the market value
-  // Decided to run with innerHTML rather than full-comp DOM building.
-  for (var i = 0, len = obj.length; i < len; i++) {
-    color = obj[i].mVal < 0 ? 'on' : 'off'
-    html += '<tr><td>' + obj[i].pName + '<br/>' + obj[i].pCode + '</td>'
-    html += '<td class="' + color + '">' + obj[i].mVal + '</td></tr>'
-    table.innerHTML = html
+  // Clears data from the main area
+  function clearData(callback) {
+    var table = el('table');
+    if (table) {
+      table.parentNode.removeChild(table);
+    }
+    callback();
   }
 
-  // Add the table to the fragment and add the fragment to the DOM
-  frag.appendChild(table)
-  el('main').appendChild(frag)
-}
+  // Build table by creating a docfrag first, populating it with table data
+  // then pinning it to the DOM tree
+  function displayData() {
+    var
+      frag = document.createDocumentFragment('frag'),
+      table = document.createElement('table'),
+      html = '', color, i, len;
+
+    // Loop over data adding to the html var
+    // changing the color appropriate to the market value
+    // Decided to run with innerHTML rather than full-comp DOM building.
+    html += '<th>Portfolio information</th><th>Market value</th>'
+    for (i = 0, len = obj.length; i < len; i++) {
+      color = (obj[i].mVal < 0) ? 'on' : 'off';
+      html += '<tr><td class="first"><span>' + obj[i].pName + '</span><span class="bold">' + obj[i].pCode + '</span></td>';
+      html += '<td class="' + color + '">' + obj[i].mVal + '</td></tr>';
+      table.innerHTML = html;
+    }
+
+    // Set the table id to 'table'
+    // Add the table to the fragment and add the fragment to the DOM
+    table.setAttribute('id', 'table');
+    frag.appendChild(table);
+    el('main').appendChild(frag);
+  }
+
+})()
